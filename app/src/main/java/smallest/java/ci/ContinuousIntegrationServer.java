65d99aaa31bade57f6c41c10a0c7770769d06189
GustavDowling <47000821+GustavDowling@users.noreply.github.com>
2021-02-10 22:37:23 +0100

    First solution for automatic building and testing #6 #5
    Currently only works with hardcoded directories on my PC and a hardcoded git to look "watch". Prints results of building and testing to stdout

--- app/src/main/java/smallest/java/ci/ContinuousIntegrationServer.java
+++ app/src/main/java/smallest/java/ci/ContinuousIntegrationServer.java
@@ -5,6 +5,10 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.ServletException;
  
 import java.io.IOException;
+import java.io.File; 
+import java.io.*;
+
+import java.util.concurrent.TimeUnit;
  
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.Request;
@@ -26,12 +30,50 @@ public class ContinuousIntegrationServer extends AbstractHandler
         switch (requestMethod){
             case "GET":
                 System.out.printf("handling %s request to URI: %s\n", requestMethod, target);
+               
                 response.setContentType("text/html;charset=utf-8");
                 response.setStatus(HttpServletResponse.SC_OK);
                 baseRequest.setHandled(true);
                 response.getWriter().println("CI job done");
+                
             case "POST":
-                System.out.printf("handling %s request to URI: %s\n", requestMethod, target);
+				System.out.printf("handling %s request to URI: %s\n", requestMethod, target);
+				
+				
+				File output = new File("/home/gustav/Desktop/prog/output");
+                Runtime.getRuntime().exec("git clone https://github.com/GustavDowling/DD2480-Software-Engineering-Fundamentals_Group-13.git", null, output);
+                
+				try {
+					TimeUnit.SECONDS.sleep(3); //sleep for 3 seconds while it downloads the files
+				}
+				catch(InterruptedException e)
+				{
+					Thread.currentThread().interrupt();
+				}
+				
+				File gitfolder = new File("/home/gustav/Desktop/prog/output/DD2480-Software-Engineering-Fundamentals_Group-13");
+				
+				Runtime.getRuntime().exec("git pull", null, gitfolder); //if git folder was already there, get a new version
+				
+				try {
+					TimeUnit.SECONDS.sleep(3); //sleep for 3 seconds while it downloads the files
+				}
+				catch(InterruptedException e)
+				{
+					Thread.currentThread().interrupt();
+				}
+				
+				//solution for getting the output from "exec" https://stackoverflow.com/questions/4741878/redirect-runtime-getruntime-exec-output-with-system-setout
+                Runtime rt = Runtime.getRuntime();
+                Process proc = rt.exec("./gradlew clean build", null, gitfolder);
+                BufferedReader stdInput = new BufferedReader(new InputStreamReader(proc.getInputStream()));
+                
+                String s = null;
+				while ((s = stdInput.readLine()) != null) {
+					System.out.println(s);
+				}
+         
+                
                 response.setStatus(HttpServletResponse.SC_OK);
                 baseRequest.setHandled(true);
         }
